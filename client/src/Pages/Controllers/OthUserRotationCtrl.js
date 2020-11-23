70f0bb1c986fdcdc968536fc3c809ebd451ec762
JSungMin <jsm1505104@gmail.com>
2020-11-23 22:47:09 +0900

    20/11/23 RotationCtrl Quaternion 오류 수정

--- client/src/Pages/Controllers/OthUserRotationCtrl.js
+++ client/src/Pages/Controllers/OthUserRotationCtrl.js
@@ -1,9 +1,11 @@
-import { Vector3, Quaternion } from "three";
+import {Vector3, Quaternion, MathUtils, Euler} from "three";
 
 const OthUserRotationCtrl = (page_sigs, socket, model, data, uid)=>{
     const netw_obj = data;
     let target_quat = new Quaternion();
 
+    let rot = new Quaternion(0,0,0,1);
+    let euler = new Euler(0,0,0);
     const ProcessServerMessage = (msg)=>{
         if(msg.entity_id !== uid) return;
         const inputQuat = new Quaternion(
@@ -12,7 +14,11 @@ const OthUserRotationCtrl = (page_sigs, socket, model, data, uid)=>{
             msg.entity_properties.quaternion._z,
             msg.entity_properties.quaternion._w
         );
-        target_quat = inputQuat;
+        rot = new Quaternion(0,inputQuat.y, 0, inputQuat.w);
+        //rot = inputQuat;
+        euler = euler.setFromQuaternion(rot);
+        euler.y += MathUtils.degToRad(180);
+
     }
     const OnInit = ()=>{
         socket.on('instance-state', ProcessServerMessage);
@@ -20,9 +26,13 @@ const OthUserRotationCtrl = (page_sigs, socket, model, data, uid)=>{
     const OnDispose = ()=>{
         socket.off('instance-state', ProcessServerMessage);
     }
+    let tempQuat = new Quaternion(0,0,0,1);
     const OnUpdate = (delta)=>{
-        if(!target_quat) return;
-        model.quaternion.slerp(target_quat, 0.5);
+        if(!rot) return;
+        model.rotation.copy(euler);
+        //model.quaternion.copy(rot);
+        console.log(model);
+        //model.quaternion.slerp(target_quat, 0.5);
     }
     page_sigs.update.add(OnUpdate);
     model.sigs.init.add(OnInit);
--- client/src/Pages/Controllers/PlayerRotationCtrl.js
+++ client/src/Pages/Controllers/PlayerRotationCtrl.js
@@ -1,9 +1,12 @@
-import { Vector3, Quaternion } from "three";
+import { Vector3, Quaternion,Euler } from "three";
+import {MathUtils} from "three";
 
 const PlayerRotationCtrl = (page_sigs, socket, model, data, uid, camera)=>{
     const netw_obj = data;
     let target_quat = new Quaternion();
 
+    let rot = new Quaternion(0,0,0,1);
+    let euler = new Euler(0,0,0);
     const ProcessServerMessage = (msg)=>{
         if(msg.entity_id !== uid) return;
         const inputQuat = new Quaternion(
@@ -12,7 +15,10 @@ const PlayerRotationCtrl = (page_sigs, socket, model, data, uid, camera)=>{
             msg.entity_properties.quaternion._z,
             msg.entity_properties.quaternion._w
         );
-        target_quat = inputQuat;
+        rot = new Quaternion(0,inputQuat.y, 0, inputQuat.w);
+        euler = euler.setFromQuaternion(rot);
+        euler.y += MathUtils.degToRad(180);
+        //rot = inputQuat;
     }
     const OnInit = ()=>{
         socket.on('instance-state', ProcessServerMessage);
@@ -20,9 +26,15 @@ const PlayerRotationCtrl = (page_sigs, socket, model, data, uid, camera)=>{
     const OnDispose = ()=>{
         socket.off('instance-state', ProcessServerMessage);
     }
+    let tempQuat = new Quaternion(0,0,0,1);
     const OnUpdate = (delta)=>{
-        if(!target_quat) return;
-        model.quaternion.slerp(target_quat, 0.5);
+        if(!rot) return;
+
+        model.rotation.copy(euler);
+        //model.quaternion.copy(rot);
+        console.log(model);
+        console.log(model.quaternion);
+        //model.quaternion.slerp(target_quat, 0.5);
     }
     page_sigs.update.add(OnUpdate);
     model.sigs.init.add(OnInit);
