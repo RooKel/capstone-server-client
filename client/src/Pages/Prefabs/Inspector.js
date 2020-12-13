571e07b9e40b5a11d75c0094939ea1eda2ad6ec8
kylekim95 <kylekim95@gmail.com>
2020-12-13 23:12:12 +0900

    12/13
    push

---
+++ client/src/Pages/Prefabs/Inspector.js
@@ -0,0 +1,6 @@
+
+const Inspector = (dest, params)=>{
+    
+}
+
+export {Inspector}
---
+++ client/src/Pages/Prefabs/ToggleVisibility.js
@@ -0,0 +1,6 @@
+
+const ToggleVisibility = (dest, params)=>{
+    dest.visible = !dest.visible;
+}
+
+export {ToggleVisibility}
