e843e89c7d58a0d94dee2d4aec936d72ccbe4fdb
kylekim95 <kylekim95@gmail.com>
2020-12-14 00:44:45 +0900

    12/14
    Inspector

--- client/src/Pages/Prefabs/Inspector.js
+++ client/src/Pages/Prefabs/Inspector.js
@@ -1,6 +1,23 @@
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 const Inspector = (dest, params)=>{
-    
+    document.addEventListener('keydown', (e)=>{
+        if(e.code === 'Escape'){
+            params.canvas.camera.position.set(0,0,0);
+            params.canvas.camera.lookAt(0,0,-1);
+            cam_ctrl.dispose();
+            params.canvas.scene.remove(clone);
+        }
+    }, {once: true});
+    params.pointer.Active(false);
+    params.camera.sigs.dispose.dispatch();
+
+    const clone = dest.clone();
+    clone.position.set(0,0,-2);
+    params.canvas.scene.add(clone);
+
+    const cam_ctrl = new OrbitControls(params.canvas.camera, document.getElementById('three-canvas'));
+    cam_ctrl.target.set(0,0,-2);
 }
 
 export {Inspector}
--- client/src/Pages/UI/Canvas.js
+++ client/src/Pages/UI/Canvas.js
@@ -1,8 +1,12 @@
-import {Scene, PerspectiveCamera} from 'three'
+import {Scene, PerspectiveCamera, DirectionalLight} from 'three'
 import ThreeMeshUI from 'three-mesh-ui'
 
 const Canvas = (page_sigs)=>{
     const scene = new Scene();
+    const light = new DirectionalLight(0xFFFFFF, 0.25);
+    light.position.set(0,1,1);
+    light.target.position.set(0,0,0);
+    scene.add(light);
     const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     page_sigs.update.add((delta)=>{
         ThreeMeshUI.update();
